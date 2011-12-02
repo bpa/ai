743db7840563acb5ce8d16be85bbf7a987d6d933
Bruce Armstrong <bruce@fortressofgeekdom.org>
2011-12-02 01:32:32 -0700

    Start with a schema to get moving

---
+++ tournament_genetics/schema.sql
@@ -0,0 +1,19 @@
+create table individual (
+  individual_id int primary key auto_increment,
+);
+
+create table genes (
+  gene_id int primary key auto_increment,
+  individual_id int not null,
+  marker int not null,
+  value text
+);
+
+create table markers (
+  marker text primary key,
+  datatype text,
+  validation text
+);
+
+insert into markers values ('distance', 'range', '1..10');
+insert into markers values ('aim', 'set', 'true,false');
